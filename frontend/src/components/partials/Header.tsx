43e83b565988d842bf0b2f38d3e5cec6b9289f8c
Daniel <dannyj172@gmail.com>
2025-12-29 23:25:09 +0200

    Fix header conditional bg

--- frontend/src/components/partials/Header.tsx
+++ frontend/src/components/partials/Header.tsx
@@ -3,10 +3,10 @@ import { useLocation } from "react-router-dom";
 const Header = () => {
   const location = useLocation();
   return (
-    <div className="flex w-full items-center justify-center absolute">
+    <div className="flex w-screen items-center justify-center absolute">
       <h1
         className={`${
-          location.pathname === "/details" ? "bg-none" : "bg-gray-500/15"
+          location.pathname === "/" ? "bg-gray-500/15" : "bg-none"
         } text-center mt-7 tracking-wider text-sm text-(--white) blur-[0.5px] cursor-default z-50 px-2 py-0.5 rounded-lg`}
       >
         MonoShare
